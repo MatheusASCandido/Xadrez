28941b1a26b6f179eac196c8945e7b2bb1626cd5
MatheusASCandido <63605727+MatheusASCandido@users.noreply.github.com>
2020-05-07 10:58:29 -0300

    Update ControleComando.java

--- src/pt/ControleComando.java
+++ src/pt/ControleComando.java
@@ -15,7 +15,6 @@ public class ControleComando extends Movimento {
 		v = new Movimento[a];
 	}
 	void preecher(String s) {
-		//System.out.println(s.length());
 		if(s.length()==5) {
 			v[tam]= new Movimento();
 			v[tam].str(s);
@@ -24,16 +23,12 @@ public class ControleComando extends Movimento {
 		else if(s.length()==1) {
 			v[tam]= new Transformacao();
 			v[tam].str(s);
-			//System.out.println(v[tam].s);
-			//System.out.println(s);
 			v[tam].movimento=false;
-			//v[tam].comando(s);
 		}
 		tam++;
 	}
 	void executar(ControleComando x) {
 		for(int i=0; i<a; i++) {
-			//System.out.println(v[i].s.length());
 			if(v[i].s.length()==5) {
 				v[i].comando(v[i].s);
 				String a = v[i].s;
@@ -46,21 +41,10 @@ public class ControleComando extends Movimento {
 				
 			}
 			else if (v[i].s.length()==1) {
-				//System.out.println(v[i].s);
 				v[i].comando(v[i].s);
-				//System.out.println(v[i].f);
 				x.transformar(v[i].f);
 			}
-			/*else if (v[i].s.length()==7){
-				v[i].comando(v[i].s);
-				String a = v[i].s;
-				String b = a.substring(0,2);
-				String c = a.substring(3,5);
-				System.out.println("Source: "+b);
-		        System.out.println("Target: "+c);
-				x.mover(v[i].i,v[i].j,v[i].m,v[i].n);
-				x.transformar(v[i].m,v[i].n,v[i].f);
-			}*/
+			
 		}
 	}
 }
